5b43f9594a695c8cd145b490dbf73c693cb4c4ba
Daniel Panhan <daniel.panhan@movile.com>
2016-01-10 16:46:44 -0200

    Functional interfaces.

--- src/main/java/com/movile/study/java8/lambda/LambaMain1.java
+++ src/main/java/com/movile/study/java8/lambda/LambaMain1.java
@@ -5,11 +5,47 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.BinaryOperator;
+import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.DoubleConsumer;
+import java.util.function.DoubleFunction;
+import java.util.function.DoublePredicate;
+import java.util.function.DoubleSupplier;
+import java.util.function.DoubleToIntFunction;
+import java.util.function.DoubleToLongFunction;
+import java.util.function.DoubleUnaryOperator;
 import java.util.function.Function;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntConsumer;
+import java.util.function.IntFunction;
+import java.util.function.IntPredicate;
+import java.util.function.IntSupplier;
+import java.util.function.IntToDoubleFunction;
+import java.util.function.IntToLongFunction;
+import java.util.function.IntUnaryOperator;
+import java.util.function.LongBinaryOperator;
+import java.util.function.LongConsumer;
+import java.util.function.LongFunction;
+import java.util.function.LongPredicate;
+import java.util.function.LongSupplier;
+import java.util.function.LongToDoubleFunction;
+import java.util.function.LongToIntFunction;
+import java.util.function.LongUnaryOperator;
+import java.util.function.ObjDoubleConsumer;
+import java.util.function.ObjIntConsumer;
+import java.util.function.ObjLongConsumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.function.ToDoubleBiFunction;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntBiFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongBiFunction;
+import java.util.function.ToLongFunction;
 import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
@@ -19,9 +55,7 @@ import java.util.stream.Stream;
 public class LambaMain1 {
 
 	public static void main(String[] args) {
-		
-		
-		
+
 		// Receive nothing and return void
 		Runnable runnable1 = () -> { System.gc(); };  // () -> void
 		Runnable runnable2 = () -> System.gc();       // () -> void
@@ -72,14 +106,63 @@ public class LambaMain1 {
 		UnaryOperator<String> unaryOperator2 = (str) -> str.toUpperCase();                       // (T) -> T
 		UnaryOperator<String> unaryOperator3 = String::toUpperCase;                              // (T) -> T
 		
+		
 		// Receive two objects and return a object of the same type
 		BinaryOperator<String> binaryOperator1 = (String str1, String str2) -> { return str1.concat(str2); };  // (T, T) -> T
 		BinaryOperator<String> binaryOperator2 = (str1, str2) -> str1.concat(str2);                            // (T, T) -> T
 		BinaryOperator<String> binaryOperator3 = String::concat;                                               // (T, T) -> T
 		Stream<String> stream3 = Stream.of(new String[] {"Ana", "Pedro", "João", "Catarina", "Francisco", "Maria" });
 		System.out.println(stream3.reduce(binaryOperator3).orElse(""));
-		
+
 		executorService.shutdown();	
+		
+		BiConsumer<String, Integer> biConsumer;    // (T, U) -> void
+		BiPredicate<String, Integer> biPredicate;  // (T, U) -> boolean
+
+		IntSupplier intSupplier;              // () -> int
+		IntConsumer intConsumer;              // (int) -> void 
+		IntPredicate intPredicate;            // (int) -> boolean
+		IntFunction<String> intFunction;      // (int) -> R
+		IntUnaryOperator intUnaryOperator;    // (int) -> int
+		IntBinaryOperator intBinaryOperator;  // (int, int) -> (int)
+		
+		ToIntFunction<String> toIntFunction;           // (T) -> int
+		ToIntBiFunction<String, Long> toIntBiFunction; // (T, U) -> int
+
+		LongSupplier longSupplier;              // () -> long
+		LongConsumer lintConsumer;              // (long) -> void 
+		LongPredicate longPredicate;            // (long) -> boolean
+		LongFunction<String> longFunction;      // (long) -> R
+		LongUnaryOperator longUnaryOperator;    // (long) -> long
+		LongBinaryOperator longBinaryOperator;  // (long, long) -> (long)
+		
+		ToLongFunction<String> toLongFunction;           // (T) -> long
+		ToLongBiFunction<String, Long> toLongBiFunction; // (T, U) -> long
+		
+		DoubleSupplier doubleSupplier;              // () -> double
+		DoubleConsumer doubleConsumer;              // (double) -> void 
+		DoublePredicate doublePredicate;            // (double) -> boolean
+		DoubleFunction<String> doubleFunction;      // (double) -> R
+		DoubleUnaryOperator doubleUnaryOperator;    // (double) -> double
+		DoubleBinaryOperator doubleBinaryOperator;  // (double, double) -> (double)
+		
+		ToDoubleFunction<String> toDoubleFunction;           // (T) -> double
+		ToDoubleBiFunction<String, Long> toDoubleBiFunction; // (T, U) -> double
+
+		ObjIntConsumer<String> objIntConsumer;       // (T, int) -> void
+		ObjLongConsumer<String> objLongConsumer;     // (T, long) -> void
+		ObjDoubleConsumer<String> objDoubleConsumer; // (T, double) -> void
+
+		IntToLongFunction intToLongFunction;        // (int) -> long
+		IntToDoubleFunction intToDoubleFunction;    // (int) -> double
+		
+		LongToIntFunction longToIntFunction;        // (long) -> int
+		LongToDoubleFunction longToDoubleFunction;  // (long) -> double
+				
+		DoubleToIntFunction doubleToIntFunction;     // (double) -> int
+		DoubleToLongFunction doubleToLongFunction;   // (double) -> long
+
+		BooleanSupplier booleanSupplier;              // () -> boolean
 	}
 	
 }
