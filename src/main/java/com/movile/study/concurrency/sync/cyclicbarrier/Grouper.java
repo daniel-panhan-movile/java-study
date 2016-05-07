2dcafd758681d1d15f5d52fdb4ec0aceff2908ab
Daniel Panhan <daniel.panhan@movile.com>
2016-05-07 14:12:58 -0300

    More Java - Concurrency examples

---
+++ src/main/java/com/movile/study/concurrency/sync/cyclicbarrier/Grouper.java
@@ -0,0 +1,27 @@
+package com.movile.study.concurrency.sync.cyclicbarrier;
+
+/**
+ * @author Daniel Panhan (daniel.panhan@movile.com)
+ */
+public class Grouper implements Runnable {
+
+    private Results results;
+
+    public Grouper(Results results) {
+        this.results = results;
+    }
+
+    @Override
+    public void run() {
+        int finalResult = 0;
+        System.out.printf("Grouper: Processing results...\n");
+
+        int data[] = results.getData();
+        for (int number : data) {
+            finalResult += number;
+        }
+
+        System.out.printf("Grouper: Total result: %d.\n", finalResult);
+    }
+
+}
