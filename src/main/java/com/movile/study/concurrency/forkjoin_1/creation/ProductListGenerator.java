2861cf392d5e23ce19ab8ec68d8f3f8ad1263546
Daniel Panhan <daniel.panhan@movile.com>
2016-05-21 23:17:50 -0300

    ForkJoinPool examples

---
+++ src/main/java/com/movile/study/concurrency/forkjoin_1/creation/ProductListGenerator.java
@@ -0,0 +1,17 @@
+package com.movile.study.concurrency.forkjoin_1.creation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductListGenerator {
+    public List<Product> generate(int size) {
+        List<Product> ret = new ArrayList<Product>();
+        for (int i = 0; i < size; i++) {
+            Product product = new Product();
+            product.setName("Product " + i);
+            product.setPrice(10);
+            ret.add(product);
+        }
+        return ret;
+    }
+}
